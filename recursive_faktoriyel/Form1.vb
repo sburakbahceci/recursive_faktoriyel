3bbeb34b538b3a50f72dde652fc000b2464835d7
sburakbahceci <safaburakbahceci29@gmail.com>
2019-12-25 15:02:39 +0300

    Proje dosyası ekle.

---
+++ recursive_faktoriyel/Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+    Private Function faktoriyel(ByVal b As Integer) As Integer
+        If b <= 1 Then
+            Return 1
+        Else
+            Return faktoriyel(b - 1) * b
+        End If
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim deger As Integer
+        deger = Val(TextBox1.Text)
+
+        Try
+            If TextBox1.Text <> " " Or TextBox1.Text <> 0 Then
+                TextBox1.Text = faktoriyel(deger)
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Lütfen boşluğu doldurunuz", "Değer Yok", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+    End Sub
+
+End Class
---
+++ recursive_faktoriyel/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+' denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+' bu öznitelik değerlerini değiştirin.
+
+' Bütünleştirilmiş kod özniteliklerinin değerlerini gözden geçirin
+
+<Assembly: AssemblyTitle("recursive_faktoriyel")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("recursive_faktoriyel")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Bu proje COM'un kullanımına sunulursa aşağıdaki GUID, typelib kimliği içindir
+<Assembly: Guid("9ac0d467-db3f-4d97-8b5b-cc6183201316")>
+
+' Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+'
+'      Ana Sürüm
+'      İkincil Sürüm 
+'      Yapı Numarası
+'      Düzeltme
+'
+' Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+' aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
